2e560ca3a55bf001f34c6e1279b4d52aa76d3a8e
Chip Overclock <coverclock@diag.com>
2018-01-05 11:07:53 -0700

    [1] Tweaks to metadata in Makefile. [2] Added generic header file most to peg in inc directory. [3] Builds.

---
+++ Codex/inc/com/diag/codex/codex.h
@@ -0,0 +1,26 @@
+/* vi: set ts=4 expandtab shiftwidth=4: */
+#ifndef _H_COM_DIAG_CODEX_CODEX_
+#define _H_COM_DIAG_CODEX_CODEX_
+
+/**
+ * @file
+ *
+ * Copyright 2018 Digital Aggregates Corporation, Colorado, USA<BR>
+ * Licensed under the terms in LICENSE.txt<BR>
+ * Chip Overclock <coverclock@diag.com><BR>
+ * https://github.com/coverclock/com-diag-codex<BR>
+ *
+ * REFERENCES
+ * 
+ * OpenSSL, documentation, <https://www.openssl.org/docs/>
+ * 
+ * J. Viega, M. Messier, P. Chandra, _Network Security with OpenSSL_, O'Reilly, 2002
+ * 
+ * J. Viega, M. Messier, _Secure Programming Cookbook_, O'Reilly, 2003
+ * 
+ * D. Barrett, R. Silverman, R. Byrnes, _SSH, The Secure Shell_, 2nd ed., O'Reilly, 2005
+ * 
+ * Ivan Ristic, _OpenSSL Cookbook_, Feisty Duck, <https://www.feistyduck.com/books/openssl-cookbook/>
+ */
+
+#endif
