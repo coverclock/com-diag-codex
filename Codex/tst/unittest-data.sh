8096035dc39218e9e3bc2de0d652be2d2b52b65e
Chip Overclock <coverclock@diag.com>
2018-01-19 13:08:16 -0700

    [1] Dialed up data in bulk UT test, but still no negotiations.

--- Codex/tst/unittest-data.sh
+++ Codex/tst/unittest-data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
+NEAREND=${1:-"49152"}
+FAREND=${2:-"localhost:${NEAREND}"}
 export COM_DIAG_DIMINUTO_LOG_MASK=0xfffe
-unittest-server -B 4096 -b 1048576 -s 60 -v  & sleep 1
-dd if=/dev/urandom bs=1024 count=1048576 iflag=fullblock | unittest-client -B 4096 -v > /dev/null
+unittest-server -n ${NEAREND} -B 4096 -b 1048576 -s 60 -v  & sleep 1
+dd if=/dev/urandom bs=8192 count=1048576 iflag=fullblock | time unittest-client -f ${FAREND} -B 4096 -v > /dev/null
 kill -TERM $!
