cb074ab526a364ac23cb8cf48eb5c88a10a54729
Chip Overclock <coverclock@diag.com>
2018-03-05 14:16:36 -0700

    [1] Changed serial number suffix from srn to srl (seems that's what others use). [2] Added some grep-able tags to some log statements. [3] Builds. [4] Handshake runs.

--- Codex/src/codex_verify.c
+++ Codex/src/codex_verify.c
@@ -80,7 +80,7 @@ int codex_verification_callback(int ok, X509_STORE_CTX * ctx)
 			X509_NAME_oneline(nam, name, sizeof(name));
 			name[sizeof(name) - 1] = '\0';
 		}
-		DIMINUTO_LOG_INFORMATION("codex_verification_callback: ctx=%p crt=%p subject[%d]=\"%s\"\n", ctx, crt, depth, name);
+		DIMINUTO_LOG_INFORMATION("codex_verification_callback: x509 ctx=%p crt=%p subject[%d]=\"%s\"\n", ctx, crt, depth, name);
 
 		name[0] = '\0';
 		nam = X509_get_issuer_name(crt);
@@ -88,7 +88,7 @@ int codex_verification_callback(int ok, X509_STORE_CTX * ctx)
 			X509_NAME_oneline(nam, name, sizeof(name));
 			name[sizeof(name) - 1] = '\0';
 		}
-		DIMINUTO_LOG_INFORMATION("codex_verification_callback: ctx=%p crt=%p issuer[%d]=\"%s\"\n", ctx, crt, depth, name);
+		DIMINUTO_LOG_INFORMATION("codex_verification_callback: x509 ctx=%p crt=%p issuer[%d]=\"%s\"\n", ctx, crt, depth, name);
 
 	}
 
@@ -96,14 +96,14 @@ int codex_verification_callback(int ok, X509_STORE_CTX * ctx)
 
 		error = X509_STORE_CTX_get_error(ctx);
 		text = X509_verify_cert_error_string(error);
-		DIMINUTO_LOG_NOTICE("codex_verification_callback: ctx=%p crt=%p error=%d=\"%s\"\n", ctx, crt, error, (text != (const char *)0) ? text : "");
+		DIMINUTO_LOG_NOTICE("codex_verification_callback: x509 ctx=%p crt=%p error=%d=\"%s\"\n", ctx, crt, error, (text != (const char *)0) ? text : "");
 
 		switch (error) {
 
 		case X509_V_ERR_DEPTH_ZERO_SELF_SIGNED_CERT:
 		case X509_V_ERR_SELF_SIGNED_CERT_IN_CHAIN:
 			if (codex_self_signed_certificates) {
-				DIMINUTO_LOG_NOTICE("codex_verification_callback: ctx=%p crt=%p self_signed_certificates=%d\n", ctx, crt, codex_self_signed_certificates);
+				DIMINUTO_LOG_NOTICE("codex_verification_callback: x509 ctx=%p crt=%p self_signed_certificates=%d\n", ctx, crt, codex_self_signed_certificates);
 				ok = 1;
 			}
 			break;
