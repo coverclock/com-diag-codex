5c5cde6bcf79eca707a9dae085ef17e829c71e03
Chip Overclock <coverclock@diag.com>
2018-03-20 12:25:30 -0600

    [1] Results of core timing test. [2] Minor improvements to time script. [3] Tested.

--- Codex/tst/unittest-time.sh
+++ Codex/tst/unittest-time.sh
@@ -4,16 +4,26 @@
 # Chip Overclock (mailto:coverclock@diag.com)
 # https://github.com/coverclock/com-diag-codex
 
+# See also: bin/unittest-time.awk that that extracts data
+#           from the resulting log file and formats it
+#           into a CSV file suitable for Excel.
+
+# The sleep delays are to accomodate the fact that
+# I can't quite seem to set the REUSE socket option
+# in OpenSSL.
+
 FIRST=1
 LAST=1048576
 
-if false; then
+if true; then
     BLOCKS=${FIRST}
     while [[ ${BLOCKS} -le ${LAST} ]]; do
         echo unittest-time-control ${BLOCKS}
         unittest-time-control ${BLOCKS}
-        sleep 10
         BLOCKS=$(( ${BLOCKS} * 2 ))
+	if [[ ${BLOCKS} -le ${LAST} ]]; then
+            sleep 10
+	fi
     done
 fi
 
@@ -22,8 +32,10 @@ if true; then
     while [[ ${BLOCKS} -le ${LAST} ]]; do
         echo unittest-time-core ${BLOCKS}
         unittest-time-core ${BLOCKS}
-        sleep 300
         BLOCKS=$(( ${BLOCKS} * 2 ))
+	if [[ ${BLOCKS} -le ${LAST} ]]; then
+            sleep 300
+	fi
     done
 fi
 
