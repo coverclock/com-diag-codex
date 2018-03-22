522cd60d00c05b619cb1d852f34e51b15f30bdd2
jsloan <jsloan@diag.com>
2018-03-22 08:14:26 -0600

    [1] Added R examples.

---
+++ Codex/tst/unittest-time-control.r
@@ -0,0 +1,9 @@
+control <- read.table("src/com-diag-codex/Codex/dat/unittest-time-control-all.csv", header=TRUE, sep=",", quote="\"")
+data <- log2(control[8]) - 12
+data[2] <- log2(control[9]) - 6
+data[3] <- control[5]
+array <- data.matrix(data)
+matrix <- matrix(nrow=7,ncol=21)
+for (ii in seq(from=1,to=147,by=1)) matrix[array[ii,2] + 1,array[ii,1] + 1] <- array[ii,3]
+log10 <- log10(matrix)
+kilolog10 <- log10 * 1000
