8a25d2389bdca06a4398e008dd378ba71962b605
jsloan <jsloan@diag.com>
2018-03-22 08:26:24 -0600

    [1] Updated R scripts. [2] Added resulting matrix CSV files.

--- Codex/tst/unittest-time-core.r
+++ Codex/tst/unittest-time-core.r
@@ -6,4 +6,4 @@ array <- data.matrix(data)
 matrix <- matrix(nrow=7,ncol=21)
 for (ii in seq(from=1,to=147,by=1)) matrix[array[ii,2] + 1,array[ii,1] + 1] <- array[ii,3]
 log10 <- log10(matrix)
-kilolog10 <- log10 * 1000
+write.table(matrix, file = "src/com-diag-codex/Codex/dat/unittest-time-core-matrix.csv", quote = TRUE, sep = ",", eol = "\n", dec = ".", row.names = FALSE, col.names = FALSE)
