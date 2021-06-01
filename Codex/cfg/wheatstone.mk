81081248b6caab94134337f471435dddb1d52df3
Chip Overclock <coverclock@diag.com>
2021-06-01 08:42:15 -0600

    [1] Added wheatstone make and config files. TESTED [1.1] Both default and wheatstone targets BUILD. [1.2] Sanity and functional unit tests PASS.

---
+++ Codex/cfg/wheatstone.mk
@@ -0,0 +1,14 @@
+# Copyright 2021 Digital Aggregates Corporation
+# Licensed under the terms in LICENSE.txt
+# author:Chip Overclock
+# mailto:coverclock@diag.com
+# https://github.com/coverclock/com-diag-codex
+# "Chip Overclock" is a registered trademark.
+# "Digital Aggregates Corporation" is a registered trademark.
+# In support of the Wheatstone sub-project in com-diag-hazer.
+
+# WHEATSTONE_ALG:	message digest cryptographic hash function for certificate signing
+# WHEATSTONE_KEY:	asymmetric encryption algorithm:key size in bits for certificate public/private keys
+
+WHEATSTONE_ALG		:=	sha256
+WHEATSTONE_KEY 		:=	rsa:3072
