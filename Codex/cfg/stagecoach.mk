80c8eba6ac580d56d60bc072e00556d62d74713e
Chip Overclock <coverclock@diag.com>
2022-11-28 12:48:00 -0700

    [1] Abstracted out application logic from Wheatstone for Stagecoach. TESTED [2] Added Stagecoach files. TESTED

--- Codex/cfg/stagecoach.mk
+++ Codex/cfg/stagecoach.mk
@@ -1,11 +1,11 @@
-# Copyright 2021-2022 Digital Aggregates Corporation
+# Copyright 2022 Digital Aggregates Corporation
 # Licensed under the terms in LICENSE.txt
 # author:Chip Overclock
 # mailto:coverclock@diag.com
 # https://github.com/coverclock/com-diag-codex
 # "Chip Overclock" is a registered trademark.
 # "Digital Aggregates Corporation" is a registered trademark.
-# In support of the Wheatstone sub-project in com-diag-hazer.
+# In support of the Stagecoach sub-project in com-diag-hazer.
 
 # APPLICATION_ALG:	message digest cryptographic hash function for certificate signing
 # APPLICATION_KEY:	asymmetric encryption algorithm:key size in bits for certificate public/private keys
