97642398e60a68d70970aba2771a9d65df7b06b7
Chip Overclock <coverclock@diag.com>
2022-11-30 10:47:34 -0700

    [1] Updated copyright in Makefile. [2] Moved wheatstone server. [3] Added stub for stagecoach. [4] Sanity, functional, and extra tests all PASS.

---
+++ Codex/app/stagecoach/main.c
@@ -0,0 +1,33 @@
+/* vi: set ts=4 expandtab shiftwidth=4: */
+/**
+ * @file
+ *
+ * Copyright 2022 Digital Aggregates Corporation, Colorado, USA<BR>
+ * Licensed under the terms in LICENSE.txt<BR>
+ * Chip Overclock (mailto:coverclock@diag.com)<BR>
+ * https://github.com/coverclock/com-diag-codex<BR>
+ * WORK IN PROGRESS
+ */
+
+#include "com/diag/diminuto/diminuto_assert.h"
+#include "com/diag/diminuto/diminuto_core.h"
+#include "com/diag/diminuto/diminuto_delay.h"
+#include "com/diag/diminuto/diminuto_fd.h"
+#include "com/diag/diminuto/diminuto_hangup.h"
+#include "com/diag/diminuto/diminuto_ipc4.h"
+#include "com/diag/diminuto/diminuto_ipc6.h"
+#include "com/diag/diminuto/diminuto_log.h"
+#include "com/diag/diminuto/diminuto_mux.h"
+#include "com/diag/diminuto/diminuto_terminator.h"
+#include "com/diag/diminuto/diminuto_tree.h"
+#include "com/diag/codex/codex.h"
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+int main(int argc, char * argv[])
+{
+    exit(0);
+}
