ce5296383ab6b908021dd16b7cb571d3f99a6f6f
Chip Overclock <coverclock@diag.com>
2023-01-16 12:53:11 -0700

    [1] Checkpoint. BUILDS

--- Codex/app/stagecoach/main.c
+++ Codex/app/stagecoach/main.c
@@ -45,27 +45,38 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include "../src/codex.h"
 
-typedef enum Role { UNKNOWN = 0, PRODUCER = 1, CONSUMER = 2, } role_t;
+typedef enum Role { UNKNOWN = 0, CLIENT = 1, SERVER = 2, } role_t;
+
+typedef uint16_t length_t;
 
 static const char * program = "stagecoach";
 static role_t role = UNKNOWN;
 static const char * nearend = (const char *)0;
 static const char * farend = (const char *)0;
 static const char * expected = (const char *)0;
-static size_t bufsize = 65527;
+static size_t bufsize = 65527; /* (2^16-1)-8 */
 static const char * pathcaf = (const char *)0;
 static const char * pathcap = (const char *)0;
 static const char * pathcrl = (const char *)0;
 static const char * pathcrt = (const char *)0;
 static const char * pathkey = (const char *)0;
 static const char * pathdhf = (const char *)0;
-static int selfsigned = -1;
+static bool selfsigned = true;
+static bool verbose = false;
 
 int main(int argc, char * argv[])
 {
-    char * endptr = (char *)0;
     int opt = '\0';
+    char * endptr = (char *)0;
+    int rc = -1;
+    length_t length = 0;
+    uint8_t * buffer = (uint8_t *)0;
+    diminuto_mux_t mux = { 0 };
+    diminuto_ipc_endpoint_t farendpoint = { 0 };
+    diminuto_ipc_endpoint_t nearendpoint = { 0 };
+    codex_context_t * ctx = (codex_context_t *)0;
     extern char * optarg;
 
     (void)diminuto_core_enable();
@@ -74,7 +85,7 @@ int main(int argc, char * argv[])
 
     program = ((program = strrchr(argv[0], '/')) == (char *)0) ? argv[0] : program + 1;
 
-    while ((opt = getopt(argc, argv, "B:C:D:K:L:P:R:SVcf:e:psv?")) >= 0) {
+    while ((opt = getopt(argc, argv, "B:C:D:K:L:MP:R:ce:f:mn:sv?")) >= 0) {
 
         switch (opt) {
 
@@ -98,6 +109,10 @@ int main(int argc, char * argv[])
         	pathcrl = (*optarg != '\0') ? optarg : (const char *)0;
         	break;
 
+        case 'M':
+        	selfsigned = true;
+        	break;
+
         case 'P':
         	pathcap = (*optarg != '\0') ? optarg : (const char *)0;
         	break;
@@ -106,12 +121,8 @@ int main(int argc, char * argv[])
         	pathcaf = (*optarg != '\0') ? optarg : (const char *)0;
         	break;
 
-        case 'S':
-        	selfsigned = 0;
-        	break;
-
         case 'c':
-            role = CONSUMER;
+            role = CLIENT;
             break;
 
         case 'e':
@@ -122,20 +133,24 @@ int main(int argc, char * argv[])
         	farend = optarg;
         	break;
 
+        case 'm':
+        	selfsigned = false;
+        	break;
+
         case 'n':
         	nearend = optarg;
         	break;
 
-        case 'p':
-            role = PRODUCER;
+        case 's':
+            role = SERVER;
             break;
 
-        case 's':
-        	selfsigned = 1;
-        	break;
+        case 'v':
+            verbose = true;
+            break;
 
         case '?':
-        	fprintf(stderr, "usage: %s [ -B BUFSIZE ] [ -C CERTIFICATEFILE ] [ -D DHPARMSFILE ] [ -K PRIVATEKEYFILE ] [ -L REVOCATIONFILE ] [ -P CERTIFICATESPATH ] [ -R ROOTFILE ] [ -e EXPECTED ] [ -f FAREND ] [ -n NEAREND ] [ -S | -s ] [ -p | -c ]\n", program);
+        	fprintf(stderr, "usage: %s [ -v ] [ -B BUFSIZE ] [ -C CERTIFICATEFILE ] [ -D DHPARMSFILE ] [ -K PRIVATEKEYFILE ] [ -L REVOCATIONFILE ] [ -P CERTIFICATESPATH ] [ -R ROOTFILE ] [ -e EXPECTEDDOMAIN ] [ -f FAREND ] [ -n NEAREND ] [ -M | -m ] [ -c | -s ]\n", program);
             return 1;
             break;
 
@@ -143,7 +158,89 @@ int main(int argc, char * argv[])
 
     }
 
-	DIMINUTO_LOG_INFORMATION("%s: BEGIN B=%zu C=\"%s\" D=\"%s\" K=\"%s\" L=\"%s\" P=\"%s\" R=\"%s\" f=\"%s\" n=\"%s\" e=\"%s\" s=%d %s\n", program, bufsize, pathcrt, pathdhf, pathkey, (pathcrl == (const char *)0) ? "" : pathcrl, (pathcap == (const char *)0) ? "" : pathcap, (pathcaf == (const char *)0) ? "" : pathcaf, (farend == (const char *)0) ? "" : farend, (nearend == (const char *)0) ? "" : nearend, (expected == (const char *)0) ? "" : expected, selfsigned, (role == PRODUCER) ? "Producer" : (role == CONSUMER) ? "Consumer" : "Unknown");
+	DIMINUTO_LOG_INFORMATION("%s: BEGIN v=%d B=%zu C=\"%s\" D=\"%s\" K=\"%s\" L=\"%s\" M=%d P=\"%s\" R=\"%s\" e=\"%s\" f=\"%s\" n=\"%s\" %s\n",
+        program,
+        verbose,
+        bufsize,
+        (pathcrt == (const char *)0) ? "" : pathcrt,
+        (pathdhf == (const char *)0) ? "" : pathdhf,
+        (pathkey == (const char *)0) ? "" : pathkey,
+        (pathcrl == (const char *)0) ? "" : pathcrl,
+        selfsigned,
+        (pathcap == (const char *)0) ? "" : pathcap,
+        (pathcaf == (const char *)0) ? "" : pathcaf,
+        (expected == (const char *)0) ? "" : expected,
+        (farend == (const char *)0) ? "" : farend,
+        (nearend == (const char *)0) ? "" : nearend,
+        (role == CLIENT) ? "Client" : (role == SERVER) ? "Server" : "Unknown");
+
+    buffer = (uint8_t *)malloc(bufsize);
+    assert(buffer != (uint8_t *)0);
+
+    assert(farend != (const char *)0);
+    rc = diminuto_ipc_endpoint(farend, &farendpoint);
+    assert(rc == 0);
+    switch (farendpoint.type) {
+    case AF_INET:
+        assert(!diminuto_ipc4_is_unspecified(&farendpoint.ipv4));
+        break;
+    case AF_INET6:
+        assert(!diminuto_ipc6_is_unspecified(&farendpoint.ipv6));
+        break;
+    default:
+        assert((farendpoint.type == AF_INET) || (farendpoint.type == AF_INET6));
+        break;
+    }
+
+    assert(nearend != (const char *)0);
+    rc = diminuto_ipc_endpoint(nearend, &nearendpoint);
+    assert(rc == 0);
+    switch (nearendpoint.type) {
+    case AF_INET:
+        assert(diminuto_ipc4_is_unspecified(&nearendpoint.ipv4));
+        break;
+    case AF_INET6:
+        assert(diminuto_ipc6_is_unspecified(&nearendpoint.ipv6));
+        break;
+    default:
+        assert((farendpoint.type == AF_INET) || (farendpoint.type == AF_INET6));
+        break;
+    }
+
+    switch (role) {
+    case CLIENT:
+        assert(farendpoint.tcp != 0);
+        assert(nearendpoint.udp != 0);
+        break;
+    case SERVER:
+        assert(farendpoint.udp != 0);
+        assert(nearendpoint.tcp != 0);
+        break;
+    default:
+        assert(role != UNKNOWN);
+        break;
+    }
+
+    rc = diminuto_hangup_install(!0);
+    assert(rc == 0);
+
+    diminuto_mux_init(&mux);
+
+    codex_set_self_signed_certificates(selfsigned ? 1 : 0);
+
+    rc = codex_initialize(pathdhf, pathcrl);
+    assert(rc == 0);
+
+    ctx = codex_client_context_new(pathcaf, pathcap, pathcrt, pathkey);
+    assert(ctx != (SSL_CTX *)0);
+
+    /**/
+
+    diminuto_mux_fini(&mux);
+
+    free(buffer);
+
+    DIMINUTO_LOG_INFORMATION("%s: END\n", program);
 
     exit(0);
 }
