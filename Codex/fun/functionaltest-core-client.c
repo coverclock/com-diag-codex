959836cc6401397466b6fb7c44478b08848ef409
Chip Overclock <coverclock@diag.com>
2025-01-29 11:22:54 -0700

    [1] Updated copyrights.

--- Codex/fun/functionaltest-core-client.c
+++ Codex/fun/functionaltest-core-client.c
@@ -2,7 +2,7 @@
 /**
  * @file
  *
- * Copyright 2018-2022 Digital Aggregates Corporation, Colorado, USA<BR>
+ * Copyright 2018-2025 Digital Aggregates Corporation, Colorado, USA<BR>
  * Licensed under the terms in LICENSE.txt<BR>
  * Chip Overclock (mailto:coverclock@diag.com)<BR>
  * https://github.com/coverclock/com-diag-codex<BR>
