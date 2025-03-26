725ed7cdafd711b1f5fe0e2ee6875cc8de750b4c
Chip Overclock <coverclock@diag.com>
2025-03-26 13:08:03 -0600

    [1] Checkpoint.

--- Codex/tst/unittest-stagecoach.sh
+++ Codex/tst/unittest-stagecoach.sh
@@ -8,10 +8,11 @@
 # which is used support of gpstool that is built on top of the Hazer and
 # Diminuto libraries. gpstool can transmit a variety of information -
 # NMEA sentences, RTK updates, or CSV PNT information, via UDP datagrams to
-# a remote system, which may also be running gpstool. This facility is used,
-# for example, with Differential GNSS systems, remote tracking applications,
-# etc. stagecoach is a go-between tool that receives UDP datagrams, tunnels
-# them through an SSL tunnel, then forwards them via UDP to the receiver.
+# a remote system, which may also be running gpstool or another utility
+# like rtktool. This facility is used, for example, with Differential GNSS
+# systems, remote tracking applications, etc. stagecoach is a go-between
+# tool that receives UDP datagrams, tunnels them through an SSL tunnel, then
+# forwards them via UDP to the receiver.
 #
 # Why not just use SSL to begin with? Because communication channels that
 # use TCP, like SSL, or even those that use third-party mechanisms like
@@ -21,14 +22,16 @@
 #
 # A stagecoach process in server mode acts as a proxy on the nearend for the
 # farend server for the producer, and another as the proxy on the farend for
-# the nearend client for the consumer.
+# the nearend client for the consumer. Confusingly, the proxy server stagecoach
+# runs in client (-c) mode, while the proxy client stagecoach runs in server
+# (-s) mode.
 #
 #                     proxy            proxy
 # PRODUCER <---UDP--> SERVER <==SSL==> CLIENT <---UDP--> CONSUMER
 # nearend             nearend          farend            farend
 #  |                   |                |                 |
-# gpstool             stagecoach       stagecoach        gpstool
-# rover                                                  base
+# gpstool             stagecoach -c    stagecoach -s     rtktool
+# rover                                                  router
 
 ################################################################################
 
@@ -47,64 +50,72 @@ XC=0
 # This password is only used for testing.
 export COM_DIAG_CODEX_SERVER_PASSWORD=st8g3c08ch
 
-################################################################################
-
-FILE1=$(mktemp ${TMPDIR:-"/tmp"}/$(basename ${0} .sh)-FILE1-XXXXXXXXXX.dat)
-FILE2=$(mktemp ${TMPDIR:-"/tmp"}/$(basename ${0} .sh)-FILE2-XXXXXXXXXX.dat)
-
-dd if=/dev/urandom bs=${BLOCKSIZE} count=${BLOCKS} iflag=fullblock of=${FILE1}
-dd if=/dev/urandom bs=${BLOCKSIZE} count=${BLOCKS} iflag=fullblock of=${FILE2}
-
-ls -l ${FILE1} ${FILE2}
-
-FILEA=$(mktemp ${TMPDIR:-"/tmp"}/$(basename ${0} .sh)-FILEA-XXXXXXXXXX.dat)
-FILEB=$(mktemp ${TMPDIR:-"/tmp"}/$(basename ${0} .sh)-FILEB-XXXXXXXXXX.dat)
-
-ls -l ${FILEA} ${FILEB}
-
-################################################################################
-
 SNEAREND=0.0.0.0:${TUNNEL}
 SFAREND=localhost:${FAREND}
-
-stagecoach -C ${CERT}/stagecoach-servercert.pem -K ${CERT}/stagecoach-serverkey.pem -P ${CERT} -f ${SFAREND} -n ${SNEAREND} -s &
-PIDSERVER=$!
-sleep 2
-
-ps -ef ${PIDSERVER}
 
 CNEAREND=0.0.0.0:${NEAREND}
 CFAREND=localhost:${TUNNEL}
 
+################################################################################
+
+FILEPRODUCERSOURCE=$(mktemp ${TMPDIR:-"/tmp"}/$(basename ${0} .sh)-FILEPRODUCERSOURCE-XXXXXXXXXX.dat)
+FILECONSUMERSOURCE=$(mktemp ${TMPDIR:-"/tmp"}/$(basename ${0} .sh)-FILECONSUMERSOURCE-XXXXXXXXXX.dat)
+
+dd if=/dev/urandom bs=${BLOCKSIZE} count=${BLOCKS} iflag=fullblock of=${FILEPRODUCERSOURCE}
+dd if=/dev/urandom bs=${BLOCKSIZE} count=${BLOCKS} iflag=fullblock of=${FILECONSUMERSOURCE}
+
+FILEPRODUCERSINK=$(mktemp ${TMPDIR:-"/tmp"}/$(basename ${0} .sh)-FILEPRODUCERSINK-XXXXXXXXXX.dat)
+FILECONSUMERSINK=$(mktemp ${TMPDIR:-"/tmp"}/$(basename ${0} .sh)-FILECONSUMERSINK-XXXXXXXXXX.dat)
+
+FILES="${FILEPRODUCERSOURCE} ${FILECONSUMERSOURCE} ${FILEPRODUCERSINK} ${FILECONSUMERSINK}"
+
+ls -l ${FILES}
+
+################################################################################
+
+stagecoach -C ${CERT}/stagecoach-servercert.pem -K ${CERT}/stagecoach-serverkey.pem -P ${CERT} -f ${SFAREND} -n ${SNEAREND} -s &
+PIDSERVER=$!
+sleep 5
+
 stagecoach -C ${CERT}/stagecoach-clientcert.pem -K ${CERT}/stagecoach-clientkey.pem -P ${CERT} -f ${CFAREND} -n ${CNEAREND} -c &
 PIDCLIENT=$!
-sleep 2
+sleep 5
 
-ps -ef ${PIDCLIENT}
+socat -u UDP4-RECV:? - > ${FILEPRODUCERSINK} &
+PIDPRODUCERSINK=$!
 
-socat -u UDP4-RECV:? - > ${FILEA}
+socat -u UDP4-RECV:? - > ${FILECONSUMERSINK} &
+PIDCONSUMERSINK=$!
 
-socat -u UDP4-RECV:? - > ${FILEB}
+cat ${FILEPRODUCERSOURCE} | shaperbuffered -p 256 -s 256 -m 256 | socat -u - UDP4-DATAGRAM:? &
+PIDPRODUCERSOURCE=$!
 
-cat ${FILE1} | shaperbuffered -p 256 -s 256 -m 256 | socat -u - UDP4-DATAGRAM:?
+cat ${FILECONSUMERSOURCE} | shaperbuffered -p 256 -s 256 -m 256 | socat -u - UDP4-DATAGRAM:? &
+PIDCONSUMERSOURCE=$!
 
-cat ${FILE2} | shaperbuffered -p 256 -s 256 -m 256 | socat -u - UDP4-DATAGRAM:?
+PIDS="${PIDCLIENT} ${PIDSERVER} ${PIDPRODUCERSINK} ${PIDCONSUMERSINK} ${PIDPRODUCERSOURCE} ${PIDPRODUCERSINK}"
+
+ps -ef ${PIDS}
+
+trap "kill -9 ${PIDS} 2> /dev/null; rm -f ${FILES}" HUP INT TERM
 
 ################################################################################
 
-trap "kill -9 ${PIDCLIENT} ${PIDSERVER} 2> /dev/null; rm -f ${FILE1} ${FILE2} ${FILEA} ${FILEB}" HUP INT TERM
+wait ${PIDPRODUCERSORUCE}
+
+wait ${PIDCONSUMERSOURCE}
+
+sleep 5
+
+kill ${PIDS} 2> /dev/null
 
 ################################################################################
 
-kill ${PIDCLIENT} ${PIDSERVER} 2> /dev/null
+diff ${FILEPRODUCERSOURCE} ${FILEPRODUCERSINK} || XC=$((${XC} + 1))
 
-################################################################################
+diff ${FILECONSUMERSOURCE} ${FILECONSUMERSINK} || XC=$((${XC} + 1))
 
-diff ${FILE1} ${FILEA} || XC=$((${XC} + 1))
-
-diff ${FILE2} ${FILEB} || XC=$((${XC} + 1))
-
-rm ${FILE1} ${FILE2} ${FILEA} ${FILEB}
+rm ${FILEPRODUCERSOURCE} ${FILECONSUMERSOURCE} ${FILEPRODUCERSINK} ${FILECONSUMERSINK}
 
 ################################################################################
 
