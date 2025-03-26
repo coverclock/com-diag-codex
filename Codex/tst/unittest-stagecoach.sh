2834fc6e35b544cbcfed93d7c086ec488fd0cdee
Chip Overclock <coverclock@diag.com>
2025-03-26 13:27:27 -0600

    [1] Checkpoint.

--- Codex/tst/unittest-stagecoach.sh
+++ Codex/tst/unittest-stagecoach.sh
@@ -3,7 +3,7 @@
 # Licensed under the terms in LICENSE.txt
 # Chip Overclock (mailto:coverclock@diag.com)
 # https://github.com/coverclock/com-diag-codex
-
+#
 # stagcoach is a tool built on top of the Codex and Diminuto libraries,
 # which is used support of gpstool that is built on top of the Hazer and
 # Diminuto libraries. gpstool can transmit a variety of information -
@@ -18,7 +18,8 @@
 # use TCP, like SSL, or even those that use third-party mechanisms like
 # Google's QUIC (which basically implements all of TCP's mechanisms in
 # user space), mess up the real-time nature of the UDP datagrams and make
-# the NMEA, RTK, PNT etc. data a lot less useful.
+# the NMEA, RTK, PNT etc. data a lot less useful. So we avoid using TCP
+# or TCP-like chanels when we can, and use them when we must.
 #
 # A stagecoach process in server mode acts as a proxy on the nearend for the
 # farend server for the producer, and another as the proxy on the farend for
@@ -26,12 +27,17 @@
 # runs in client (-c) mode, while the proxy client stagecoach runs in server
 # (-s) mode.
 #
-#                     proxy            proxy
-# PRODUCER <---UDP--> SERVER <==SSL==> CLIENT <---UDP--> CONSUMER
-# nearend             nearend          farend            farend
-#  |                   |                |                 |
-# gpstool             stagecoach -c    stagecoach -s     rtktool
-# rover                                                  router
+# nearend            nearend         farend           farend
+#                    proxy           proxy
+# PRODUCER ---UDP--> SERVER ==SSL==> CLIENT ---UDP--> CONSUMER
+#  |                  |               |                |
+# gpstool            stagecoach -c   stagecoach -s    rtktool
+# rover                                                 router
+#
+# REFERENCES
+#
+# Chip Overclock, "Better Never Than Late", 2017-02-16,
+# <https://coverclock.blogspot.com/2017/02/better-never-than-late.html>
 
 ################################################################################
 
@@ -50,50 +56,41 @@ XC=0
 # This password is only used for testing.
 export COM_DIAG_CODEX_SERVER_PASSWORD=st8g3c08ch
 
-SNEAREND=0.0.0.0:${TUNNEL}
-SFAREND=localhost:${FAREND}
+SERVERNEAREND=0.0.0.0:${TUNNEL}
+SERVERFAREND=localhost:${FAREND}
 
-CNEAREND=0.0.0.0:${NEAREND}
-CFAREND=localhost:${TUNNEL}
+CLIENTNEAREND=0.0.0.0:${NEAREND}
+CLIENTFAREND=localhost:${TUNNEL}
 
 ################################################################################
 
 FILEPRODUCERSOURCE=$(mktemp ${TMPDIR:-"/tmp"}/$(basename ${0} .sh)-FILEPRODUCERSOURCE-XXXXXXXXXX.dat)
-FILECONSUMERSOURCE=$(mktemp ${TMPDIR:-"/tmp"}/$(basename ${0} .sh)-FILECONSUMERSOURCE-XXXXXXXXXX.dat)
 
 dd if=/dev/urandom bs=${BLOCKSIZE} count=${BLOCKS} iflag=fullblock of=${FILEPRODUCERSOURCE}
-dd if=/dev/urandom bs=${BLOCKSIZE} count=${BLOCKS} iflag=fullblock of=${FILECONSUMERSOURCE}
 
-FILEPRODUCERSINK=$(mktemp ${TMPDIR:-"/tmp"}/$(basename ${0} .sh)-FILEPRODUCERSINK-XXXXXXXXXX.dat)
 FILECONSUMERSINK=$(mktemp ${TMPDIR:-"/tmp"}/$(basename ${0} .sh)-FILECONSUMERSINK-XXXXXXXXXX.dat)
 
-FILES="${FILEPRODUCERSOURCE} ${FILECONSUMERSOURCE} ${FILEPRODUCERSINK} ${FILECONSUMERSINK}"
+FILES="${FILEPRODUCERSOURCE} ${FILECONSUMERSINK}"
 
 ls -l ${FILES}
 
 ################################################################################
 
-stagecoach -C ${CERT}/stagecoach-servercert.pem -K ${CERT}/stagecoach-serverkey.pem -P ${CERT} -f ${SFAREND} -n ${SNEAREND} -s &
+stagecoach -C ${CERT}/stagecoach-servercert.pem -K ${CERT}/stagecoach-serverkey.pem -P ${CERT} -f ${SERVERFAREND} -n ${SERVERNEAREND} -s &
 PIDSERVER=$!
 sleep 5
 
-stagecoach -C ${CERT}/stagecoach-clientcert.pem -K ${CERT}/stagecoach-clientkey.pem -P ${CERT} -f ${CFAREND} -n ${CNEAREND} -c &
+stagecoach -C ${CERT}/stagecoach-clientcert.pem -K ${CERT}/stagecoach-clientkey.pem -P ${CERT} -f ${CLIENTFAREND} -n ${CLIENTNEAREND} -c &
 PIDCLIENT=$!
 sleep 5
 
-socat -u UDP4-RECV:? - > ${FILEPRODUCERSINK} &
-PIDPRODUCERSINK=$!
-
-socat -u UDP4-RECV:? - > ${FILECONSUMERSINK} &
+socat -u UDP4-RECV:${FAREND} - > ${FILECONSUMERSINK} &
 PIDCONSUMERSINK=$!
 
-cat ${FILEPRODUCERSOURCE} | shaperbuffered -p 256 -s 256 -m 256 | socat -u - UDP4-DATAGRAM:? &
+cat ${FILEPRODUCERSOURCE} | shaperbuffered -p 256 -s 256 -m 256 | socat -u - UDP4-DATAGRAM:${SERVERFAREND} &
 PIDPRODUCERSOURCE=$!
 
-cat ${FILECONSUMERSOURCE} | shaperbuffered -p 256 -s 256 -m 256 | socat -u - UDP4-DATAGRAM:? &
-PIDCONSUMERSOURCE=$!
-
-PIDS="${PIDCLIENT} ${PIDSERVER} ${PIDPRODUCERSINK} ${PIDCONSUMERSINK} ${PIDPRODUCERSOURCE} ${PIDPRODUCERSINK}"
+PIDS="${PIDCLIENT} ${PIDSERVER} ${PIDCONSUMERSINK} ${PIDPRODUCERSOURCE}"
 
 ps -ef ${PIDS}
 
@@ -102,8 +99,6 @@ trap "kill -9 ${PIDS} 2> /dev/null; rm -f ${FILES}" HUP INT TERM
 ################################################################################
 
 wait ${PIDPRODUCERSORUCE}
-
-wait ${PIDCONSUMERSOURCE}
 
 sleep 5
 
